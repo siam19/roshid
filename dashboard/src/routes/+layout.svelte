8a4b3e242a1dc326a846db26cc8f4b7b4468acd0
Siam Hasan <siam.hasan.19@gmail.com>
2025-06-18 08:41:30 +0600

    svelte dashboard auth

---
+++ dashboard/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import { ClerkProvider } from 'svelte-clerk/client';
+    import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+
+    const { children }: { children: Snippet } = $props();
+</script>
+
+<ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    {@render children()}
+</ClerkProvider>
---
+++ dashboard/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { SignedIn, SignedOut, SignInButton, UserButton } from 'svelte-clerk/client';
+</script>
+
+
+    
+	<header>
+		<SignedOut>
+            Sign In:
+			<SignInButton />
+		</SignedOut>
+		<SignedIn>
+            Heyy
+			<UserButton />
+		</SignedIn>
+	</header>
